48ac066427df6ba607d54844270d82056d1c4f0a
Timoc Ovidiu-Mario <ovidiu_mario.timoc@stud.acs.upb.ro>
2025-12-15 00:39:03 +0200

    Fix counter

--- PWMGEN/counter.v
+++ PWMGEN/counter.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/15/2025 02:32:57 PM
-// Design Name: 
-// Module Name: counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module counter(
      // peripheral clock signals
@@ -32,57 +12,54 @@ module counter(
     input upnotdown,
     input[7:0] prescale
     );
-      //Creem registri pentru blocurile always si countere pentru a numara cati biti mai avem de citit/scris
+    
     reg[15:0] count_val_r;
-    assign count_val=count_val_r-1;
-    //counter pana la atingerea valorii de prescale
-    reg[15:0] inner_counter=16'h0000;
-    //counter period pentru prescale la semnalul de ceas
-    wire[15:0] ss=(16'b0000000000000001<<prescale);
+    assign count_val = count_val_r; 
+    
+    // counter pana la atingerea valorii de prescale
+    reg[15:0] inner_counter;
+    
+    // counter period pentru prescale la semnalul de ceas
+    wire[15:0] ss = (16'd1 << prescale);
+
     always@(posedge clk or negedge rst_n) begin
-    if(!rst_n | count_reset) begin
-        inner_counter<=16'h0000;
-        if(upnotdown)
-            count_val_r <= 16'h0001;
-        else count_val_r<=period;
+        if(!rst_n | count_reset) begin
+            inner_counter <= 16'h0000;
+            count_val_r   <= 16'h0000;
         end
-    else begin
-        // Here should be the rest of the implementation
-        if(en) begin
-            if(upnotdown) begin
-            if(count_val_r==period &&inner_counter>=ss-1) begin //resetarea counter-ului de la prima valoare
-                count_val_r<=16'h0001;
-                inner_counter<=16'h0000;
-                end
-                else begin
-                    if(inner_counter>=ss-1) begin //resetarea counter-ului intern la atingerea valorii prescale
-                        count_val_r<=count_val_r+1'b1; //incrementare counter
-                        inner_counter<=16'h0000;
+        else begin
+            if(en) begin
+                if(upnotdown) begin // Count Up
+                    // Daca am ajuns la prescale
+                    if(inner_counter >= ss - 1) begin
+                        inner_counter <= 16'h0000;
+                        
+                        // Logica Count Up
+                        if(count_val_r >= period) 
+                            count_val_r <= 16'h0000;
+                        else 
+                            count_val_r <= count_val_r + 1'b1;
                     end
-                    else begin  //incrementarea counter intern
-                        inner_counter<=inner_counter+1'b1;
+                    else begin // incrementarea counter intern
+                        inner_counter <= inner_counter + 1'b1;
+                    end
+                end
+                else begin // Count Down
+                    // Daca am ajuns la prescale
+                    if(inner_counter >= ss - 1) begin
+                        inner_counter <= 16'h0000;
+                        
+                        // Logica Count Down
+                        if(count_val_r == 16'h0000) 
+                            count_val_r <= period;
+                        else 
+                            count_val_r <= count_val_r - 1'b1;
+                    end
+                    else begin
+                        inner_counter <= inner_counter + 1'b1;
                     end
                 end
             end
-            else begin 
-                if(count_val_r==16'h0001 &&inner_counter>=ss-1) begin //resetarea counter-ului de la prima valoare
-                count_val_r<=period;
-                inner_counter<=16'h0000;
-                end
-                else begin
-                    if(inner_counter>=ss-1) begin //resetarea counter-ului intern la atingerea valorii prescale
-                        count_val_r<=count_val_r-1'b1; //decrementare counter
-                        inner_counter<=16'h0000;
-                    end
-                    else begin  //incrementarea counter intern
-                        inner_counter<=inner_counter+1'b1;
-                    end
-                end
-            
-            end            
-             
         end
     end
-end
-
 endmodule
